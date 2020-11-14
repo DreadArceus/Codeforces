429099f86ef3eb6da392b413ab633a4915a4b7c6
Mayank Talwar <dcmtalwar@gmail.com>
2020-11-14 09:40:58 +0530

    upsolved C

--- Contests/Division 2/Round #682/C.cpp
+++ Contests/Division 2/Round #682/C.cpp
@@ -1,53 +1,10 @@
+//Recursion and even checking adjacent cells, was never needed.
+//Just do a generalisation that makes everything adjacent, different.
+//The following is basically like colouring the whole grid like a chessboard,
+//  so all adjacent cells are even if the center is odd and vice-versa
 #include <iostream>
 #include <vector>
 using namespace std;
-
-static vector<pair<int, int>> adj = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-
-bool valid(vector<vector<int>> v, vector<vector<int>> m)
-{
-    for(int i = 0; i < m.size(); i++)
-    {
-        for(int j = 0; j < m[0].size(); j++)
-        {
-            for(auto z : adj)
-            {
-                if(((i > z.first + 1 && i > z.second + 1) || z.second > 0) &&
-                    ((i < v.size() - z.first && j < v[0].size() - z.second) || z.second < 0) &&
-                    v[i][j] + m[i][j] == v[i + z.first][j + z.second] + m[i + z.first][j + z.second])
-                {
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
-
-vector<vector<int>> recurse(vector<vector<int>> v, vector<vector<int>> mod, int i, int j)
-{
-    for(auto z : adj)
-    {
-        if(((i > z.first + 1 && j > z.second + 1) || z.second > 0) &&
-            ((i < v.size() - z.first && j < v[0].size() - z.second) || z.second < 0) &&
-            v[i][j] + mod[i][j] == v[i + z.first][j + z.second] + mod[i + z.first][j + z.second])
-        {
-            vector<vector<int>> loc = mod;
-            loc[i][j]++;
-            loc = recurse(v, loc, ((j == v[0].size() - 1) ? 1 : 0) + i, ((j == v[0].size() - 1) ? -j : 1) + j);
-            if(loc[0][0] != -2)
-            {
-                return loc;
-            }
-        }
-    }
-    // if(!valid(v, mod))
-    // {
-    //     mod[0][0] = -2;
-    // }
-    return mod;
-}
-
 int main()
 {
     int t = 0;
@@ -62,15 +19,24 @@ int main()
             for(int j = 0; j < m; j++)
             {
                 cin >> v[i][j];
+                if((i + j) % 2 == 0)
+                {
+                    if(v[i][j] % 2 != 0)
+                    {
+                        v[i][j]++;
+                    }
+                }
+                else if(v[i][j] % 2 == 0)
+                {
+                    v[i][j]++;
+                }
             }
         }
-        vector<vector<int>> mod(n, vector<int>(m, 0));
-        mod = recurse(v, mod, 0, 0);
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
-                cout << v[i][j] + mod[i][j] << " ";
+                cout << v[i][j] << " ";
             }
             cout << "\n";
         }
