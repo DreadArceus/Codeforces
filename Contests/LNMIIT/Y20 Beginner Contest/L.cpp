29e09aeeddbf898531bdb1a5a14b9dfd8ade12ce
dreadarceus <dreadarceus@gmail.com>
2021-04-21 09:54:01 +0530

    Upsolve: L, Y20 beginner contest

--- Contests/LNMIIT/Y20 Beginner Contest/L.cpp
+++ Contests/LNMIIT/Y20 Beginner Contest/L.cpp
@@ -1,78 +1,53 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
 using namespace std;
+#define int long long
+#define pii pair<int, int>
 
-class Graph
-{
-private:
-    int numOfVertices;
-    vector<int> *vertices;
-    int *nonLoop;
-    void loopLengthDFS(int start, vector<bool> visited);
-public:
-    Graph(int numOfVertices): numOfVertices(numOfVertices)
-    {
-        this->vertices = new vector<int>[this->numOfVertices];
-        this->nonLoop = new int[this->numOfVertices];
-    }
-    void addEdge(int v, int w) { vertices[v].push_back(w); }
-    int* longestLoop();
-};
+vector<int> graph;
+vector<int> reversedGraph;
 
-void Graph::loopLengthDFS(int start, vector<bool> visited)
-{
-    visited[start] = 1;
-    int adjacent;
-    for(int i = 0; i < this->vertices[start].size(); i++)
-    {
-        adjacent = this->vertices[start][i];
-        if(!visited[adjacent])
-        {
-            this->loopLengthDFS(adjacent, visited);
-        }
-        else
-        {
-            this->nonLoop[start] = 1;
-        }
-    }
-}
-
-int* Graph::longestLoop()
-{
-    for(int i = 0; i < this->numOfVertices; i++)
-    {
-        vector<bool> visited(this->numOfVertices + 1, 0);
-        this->loopLengthDFS(i, visited);
-    }
-    return this->nonLoop;
-}
-
-int main()
+void solveCase()
 {
     int n = 0, m = 0;
     cin >> n >> m;
-    Graph *cities = new Graph(n);
-    for(int i = 0; i < m; i++)
+    graph.resize(n, -1);
+    reversedGraph.resize(n, -1);
+    while (m--)
     {
-        int x = 0, y = 0;
-        cin >> x >> y;
-        cities->addEdge(x - 1, y - 1);
+        int u = 0, v = 0;
+        cin >> u >> v;
+        graph[u - 1] = v - 1;
+        reversedGraph[v - 1] = u - 1;
     }
-    int *v = cities->longestLoop();
-    int count = 0;
-    vector<int> ans(0);
-    for(int i = 0; i < n; i++)
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
     {
-        if(v[i] == 0)
+        if(reversedGraph[i] != -1) continue;
+        int curr = i;
+        while(curr != -1)
         {
-            count++;
-            ans.push_back(i + 1);
+            ans.push_back(curr);
+            curr = graph[curr];
         }
     }
-    cout << count << "\n";
-    for(int i = 0; i < ans.size(); i++)
+    sort(ans.begin(), ans.end());
+    cout << ans.size() << "\n";
+    for(auto i : ans)
     {
-        cout << ans[i] << " ";
+        cout << i + 1 << " ";
     }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solveCase();
 }
