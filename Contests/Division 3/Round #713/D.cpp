272c3e0c76bddd234cb44a106c5ab95a5f3ef2bf
dreadarceus <dreadarceus@gmail.com>
2021-05-05 15:18:28 +0530

    Upsolve: D, div 3 713

--- Contests/Division 3/Round #713/D.cpp
+++ Contests/Division 3/Round #713/D.cpp
@@ -1,61 +1,74 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <set>
+#include <map>
 #include <algorithm>
+#include <cmath>
+#include <cstring>
 using namespace std;
+#define int long long
+#define pii pair<int, int>
+int32_t mod = 1e9 + 7;
+
+int n, m, x, k, l, r;
+bool f1, f2, f3;
+string s, s1, s2;
+vector<int> v, v1, v2;
+set<int> se, se1, se2;
+map<int, int> mp1, mp2;
+void initiateCase()
+{
+    n = m = x = k = l = r = 0;
+    f1 = f2 = f3 = false;
+    s = s1 = s2 = string();
+    v = v1 = v2 = vector<int>();
+    se = se1 = se2 = set<int>();
+    mp1 = mp2 = map<int, int>();
+}
+
+void solveCase()
+{
+    initiateCase();
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n + 2; i++)
+    {
+        cin >> k;
+        sum += k;
+        se.insert(k);
+        mp1[k]++;
+    }
+    for (auto it : se)
+    {
+        x = it;
+        if ((sum - x) & 1)
+            continue;
+        m = (sum - x) / 2;
+        if (mp1[m] > (x == m))
+        {
+            f1 = true;
+            mp1[m]--;
+            mp1[x]--;
+            break;
+        }
+    }
+    if (!f1)
+    {
+        cout << "-1\n";
+        return;
+    }
+    for (auto it : mp1)
+        while (it.second--)
+            cout << it.first << " ";
+    cout << "\n";
+}
+
 int32_t main()
 {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int t = 0;
     cin >> t;
     while (t--)
-    {
-        int n = 0;
-        cin >> n;
-        vector<long long> v(n + 2);
-        long long sum = 0;
-        for (int i = 0; i < n + 2; i++)
-        {
-            cin >> v[i];
-            sum += v[i];
-        }
-        sort(v.begin(), v.end());
-        bool found = false;
-        for (int i = 0; i < n + 2; i++)
-        {
-            if ((sum - v[i]) % 2 == 0)
-            {
-                long long localSum = sum - v[i];
-                vector<long long>::iterator it = lower_bound(v.begin(), v.end(), localSum / 2);
-                if (it != v.end() && *it == localSum / 2)
-                {
-                    if(it == v.begin() + i)
-                    {
-                        it++;
-                        if(it == v.end() || *it != *(v.begin() + i))
-                        {
-                            continue;
-                        }
-                    }
-                    int ind = it - v.begin();
-                    if(ind > i)
-                    {
-                        ind--;
-                    }
-                    v.erase(v.begin() + i);
-                    v.erase(v.begin() + ind);
-                    found = true;
-                    break;
-                }
-            }
-        }
-        if (!found)
-        {
-            cout << "-1\n";
-            continue;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            cout << v[i] << " ";
-        }
-        cout << "\n";
-    }
+        solveCase();
 }
