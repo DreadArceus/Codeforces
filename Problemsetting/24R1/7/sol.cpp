e34d0ff9535987c2dc8e1f92fbc466223e6c8b4f
dreadarceus <dreadarceus@gmail.com>
2021-06-15 07:31:10 +0530

    Optimized sol

--- Problemsetting/24R1/7/sol.cpp
+++ Problemsetting/24R1/7/sol.cpp
@@ -1,30 +1,20 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <cmath>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define pii pair<int, int>
-int32_t mod = 1e9 + 7;
 
-struct segTreeItem
+struct SegTreeItem
 {
     int element;
 };
 
-class segTree
+class SegTree
 {
-public:
-    segTree(int n)
+  public:
+    SegTree(int n)
     {
         this->nodes.resize(4 * n + 5, this->null);
         this->size = n;
     }
-    void pointUpdate(int x, segTreeItem val, int index, int l, int r)
+    void pointUpdate(int x, SegTreeItem val, int index, int l, int r)
     {
         if (x < l || x >= r)
             return;
@@ -37,35 +27,30 @@ public:
         pointUpdate(x, val, 2 * index + 1, (r + l) / 2, r);
         nodes[index] = merge(nodes[2 * index], nodes[2 * index + 1]);
     }
-    segTreeItem query(int x, int y, int index, int l, int r)
+    SegTreeItem query(int x, int y, int index, int l, int r)
     {
         if (y <= l || x >= r)
             return this->null;
         if (l >= x && r <= y)
             return nodes[index];
-        return merge(query(x, y, 2 * index, l, (r + l) / 2),
-                     query(x, y, 2 * index + 1, (r + l) / 2, r));
-    }
-    void pointUpdate(int x, segTreeItem val)
-    {
-        pointUpdate(x, val, 1, 0, size);
+        return merge(query(x, y, 2 * index, l, (r + l) / 2), query(x, y, 2 * index + 1, (r + l) / 2, r));
     }
     void pointUpdate(int x, int val)
     {
         pointUpdate(x, {val}, 1, 0, size);
     }
-    segTreeItem query(int x, int y)
+    SegTreeItem query(int x, int y)
     {
         return query(x, y, 1, 0, size);
     }
 
-private:
-    vector<segTreeItem> nodes;
-    segTreeItem null = {0};
+  private:
+    vector<SegTreeItem> nodes;
+    SegTreeItem null = {0};
     int size;
-    segTreeItem merge(segTreeItem a, segTreeItem b)
+    SegTreeItem merge(SegTreeItem a, SegTreeItem b)
     {
-        segTreeItem result;
+        SegTreeItem result;
         result.element = a.element + b.element;
         return result;
     }
@@ -73,16 +58,17 @@ private:
 
 void solveCase()
 {
+    auto isVowel = [](char c) { return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; };
+
     int n = 0, q = 0;
     string s;
     cin >> n >> s >> q;
-    segTree st(n);
+
+    SegTree st(n);
     for (int i = 0; i < n; i++)
-    {
-        char c = s[i];
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        if (isVowel(s[i]))
             st.pointUpdate(i, 1);
-    }
+
     while (q--)
     {
         int type = 0;
@@ -91,23 +77,20 @@ void solveCase()
         {
             int l = 0, r = 0;
             cin >> l >> r;
-            cout << st.query(l - 1, r).element << endl;
+            cout << st.query(l - 1, r).element << '\n';
         }
         else
         {
             int i = 0;
             char c;
             cin >> i >> c;
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-                st.pointUpdate(i - 1, 1);
-            else
-                st.pointUpdate(i - 1, 0);
+            st.pointUpdate(i - 1, isVowel(c));
         }
     }
 }
 
 int32_t main()
 {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false), cin.tie(NULL);
     solveCase();
 }
